923619508e9d113eb9461c44446394fa5d9a9529
JonesSodaOP <148819585+JonesSodaOP@users.noreply.github.com>
2023-10-23 17:01:16 -0500

    Add files via upload

---
+++ lab2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+cout << "*********************************" << endl; 
+cout << "\t\t Austin Jones" << endl; // First and Last name with formatting
+cout << "\t\t\tCSE 100" << endl; // Current Course with formatting
+cout << "\tWelcome to C++ Programming" << endl; // Welcome Message with formatting
+cout << "*********************************" << endl;
+cout <<endl; // Spacing
+cout <<endl; // Spacing
+  cout << "1. The sum of 2 + 3 \t\t\t\t = " << 2 + 3 << endl; // Added tabs to adjust format
+  cout << "2. The multiplication of 5*6 \t\t = " << 5*6 << endl; // Added tabs to adjust format
+  cout << "3. When I divide 15/7, the quotient  = " << 15/7 << endl; // Added tabs to adjust format
+  cout << "4. Th remainder of 15 % 4 \t\t\t = " << 15 % 4 << endl; // Added tabs to adjust format
+  cout <<endl; // Spacing
+  cout <<endl; // Spacing
+  cout << "This is the end of my first Program" << endl;
+  cout << "\t\t\tThank you!" << endl; // Added tabs to adjust format
+
+  return 0;
+}
